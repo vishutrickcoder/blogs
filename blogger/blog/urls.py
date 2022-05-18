534912b2aa93fc80a9a908851053a256211db7a4
Vishu <vs1015242@gmail.com>
2022-05-18 11:40:13 +0530

    Update

--- blogger/blog/urls.py
+++ blogger/blog/urls.py
@@ -5,11 +5,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home,post,category
+from .views import home, post, category
 
 urlpatterns = [
-    path('home/', home) ,
-    path('blog/<slug:url>',post),
-    path('category/<slug:url>',category)
+    path('', home),
+    path('blog/<slug:url>', post),
+    path('category/<slug:url>', category)
 
-     ]
+]
